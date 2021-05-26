7850c71660f8f91bd74cf0238fcd754c9b2dda7d
Piyush Pandey <piyushpvns@gmail.com>
2021-05-26 17:54:23 +0530

    Recursion
    Given the code to find out and return the number of digits present in a number recursively. But it contains few bugs, that you need to rectify such that all the test cases should pass.

---
+++ Lec3NoofDigits.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int count(int n)
+{
+    if (n == 0 || n < 10)
+    {
+        return 1;
+    }
+    int smallAns = count(n / 10);
+    return smallAns + 1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << count(n) << endl;
+}
