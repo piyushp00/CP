71ba08a9140c430c7f06c7d9d109c64eb27eecc9
Piyush Pandey <piyushpvns@gmail.com>
2021-06-05 16:10:53 +0530

    Strings

--- Advanced Recursion/Strings.cpp
+++ Advanced Recursion/Strings.cpp
@@ -11,7 +11,7 @@ int main(){
 
     string s ="abc";
     //cin terminates taking input if it detects
-    getline(cin, s);
+    //getline(cin, s);
     cout << s << endl;
 
     s = "defdef";
@@ -20,12 +20,24 @@ int main(){
     string s1;
     s1 = "def";
 
-    
+    string s2 = s + s1;
+    cout << s2 << endl;
+
+    s += s1;
 
 
-    string s1;
-    s1 = "def";
-    cout << s1 << endl;
+    string s3;
+    s3 = "def";
+    cout << s3 << endl;
     cout << s << endl;
 
+    cout << s.size() << endl;
+    cout << s.length() << endl;  // Both are same
+    
+    //Substring Function - it gives part of the string
+    cout << s.substr(3) << endl;
+    cout << s.substr(3, 3) << endl; //From third, upto third
+    
+    //find function
+    cout << s.find("def");
 }
