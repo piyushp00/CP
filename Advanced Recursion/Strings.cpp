15d0466e32789ea43bef923092631f31a66c73b8
Piyush Pandey <piyushpvns@gmail.com>
2021-06-05 15:56:53 +0530

    Strings

--- Advanced Recursion/Strings.cpp
+++ Advanced Recursion/Strings.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-#include <cstring>
+// #include <cstring>
 using namespace std;
 
 int main(){
+    //Dynamically
+    string *sp = new string;
+    *sp = "def";
+    cout << sp << endl;
+    cout << *sp << endl;
+
     string s ="abc";
+    //cin terminates taking input if it detects
+    getline(cin, s);
+    cout << s << endl;
+
+    s = "defdef";
+    cout << s[0] << endl;
+    s[0] = 'a';
+    string s1;
+    s1 = "def";
+
+    
+
+
+    string s1;
+    s1 = "def";
+    cout << s1 << endl;
     cout << s << endl;
 
 }
