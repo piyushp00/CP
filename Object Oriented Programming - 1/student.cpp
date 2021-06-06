cf83d2a2fd102cdbf32644d9b82dc5005370816b
Piyush Pandey <piyushpvns@gmail.com>
2021-06-06 18:20:08 +0530

    OOPS

--- Object Oriented Programming - 1/student.cpp
+++ Object Oriented Programming - 1/student.cpp
@@ -1,5 +1,14 @@
-class student{
+class student
+{
+public:
     int age;
+
+private:
     int rollNumber;
-    
+
+public:
+    void display()
+    {
+        cout << age  << " " << rollNumber << endl;
+    }
 };
--- Object Oriented Programming - 1/studentUse.cpp
+++ Object Oriented Programming - 1/studentUse.cpp
@@ -13,8 +13,18 @@ int main()
     s1.age = 24;
     s1.rollNumber = 101;
 
-    
+    cout << s1.age << endl;
+    cout << s1.rollNumber << endl;
 
+
+    s1.display();
+    s2.display();
     //Create objects dynamically
     student *s6 = new student;
-}
+    (*s6).age = 52;
+    (*s6).rollNumber = 84;
+
+    // Alternate way of assigning value;
+    s6->age = 21;
+    s6->rollNumber = 18;
+} 
