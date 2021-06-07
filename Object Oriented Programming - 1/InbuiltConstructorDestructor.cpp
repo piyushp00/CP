dee113cb57d7e6f4e1461cb685292e4dd053552b
Piyush Pandey <piyushpvns@gmail.com>
2021-06-07 15:23:14 +0530

    Copy Constructor

--- Object Oriented Programming - 1/InbuiltConstructorDestructor.cpp
+++ Object Oriented Programming - 1/InbuiltConstructorDestructor.cpp
@@ -3,6 +3,10 @@ using namespace std;
 #include "student.cpp"
 
 int main(){
+
+
+    
+    
     student s1(10, 1001);
     cout << "S1 : ";
     s1.display();
@@ -10,5 +14,18 @@ int main(){
     student s2(s1);
     cout << "S2 : ";
     s2.display();
+
+    student *s3 = new student(20, 2001);
+    cout << " S3 : ";
+    s3->display();
+
+    student s4(*s3);
+    cout << " S4 : ";
+    s4.display();
+
+    student *s5 = new student(*s3);
+    student *s6 = new student(s1);
+
+
 
 }
