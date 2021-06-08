ff0c9185e60996cfb4111af584a495c206937c86
Piyush Pandey <piyushpvns@gmail.com>
2021-06-08 16:17:12 +0530

    Complex Number Class

---
+++ Object Oriented Programming - 1/complexNumber.cpp
@@ -0,0 +1,35 @@
+
+
+class complexNumber
+{
+private:
+    int real;
+    int imaginary;
+
+public:
+    complexNumber(int real, int imaginary){
+        this->real = real;
+        this->imaginary= imaginary;
+    }
+
+    void print(){
+        cout << real << " + " << imaginary << "i" << endl;
+    }
+
+    void plus(complexNumber c2){
+        real = this->real + c2.real; 
+        imaginary = this->imaginary + c2.imaginary;
+
+    }
+
+    void multiply(complexNumber c2){
+        int real1 = this->real * c2.real;
+        int imaginary1 = this->real * c2.imaginary;
+        int imaginary2 = this->imaginary * c2.real;
+        int iSqr = -1;
+        int realImaginary = this->imaginary * c2.imaginary * iSqr;
+        
+        real = real1 + (realImaginary);
+        imaginary = imaginary1 + imaginary2;
+    }
+};
--- test.cpp
+++ test.cpp
@@ -1,88 +1,10 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void merge(int a[], int si, int ei)
-{
-    int size = ei - si + 1;
-    int mid = (si + ei) / 2;
-
-    int *out = new int[size];
-    int i = si;
-    int j = mid + 1;
-    int k = 0;
-
-    while (i <= mid && j <= ei)
-    {
-        if (a[i] < a[j])
-        {
-            out[k] = a[i];
-            k++;
-            i++;
-        }
-        else
-        {
-            out[k] = a[j];
-            k++;
-            j++;
-        }
-    }
-
-    while (i <= mid)
-    {
-        out[k] = a[i];
-        k++;
-        i++;
-    }
-
-    while (j <= ei)
-    {
-        out[k] = a[j];
-        k++;
-        j++;
-    }
-
-    int m = 0;
-    for (int i = 0; i < size; i++)
-    {
-        a[i] = out[m];
-        m++;
-    }
-
-    delete[] out;
-}
-
-void mergeSort(int a[], int si, int ei)
-{
-
-    //Base Case
-    if (si >= ei)
-    {
-        return;
-    }
-
-    int mid = (si + ei) / 2;
-    mergeSort(a, si, mid);
-    mergeSort(a, mid + 1, ei);
-    merge(a, si, ei);
-}
-
-int main()
-{
-    int n, si, ei;
-    int a[1000];
-
-    cin >> n >> si >> ei;
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-
-    mergeSort(a, si, ei);
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+int main(){
+    int a = pow(10, 200);
+    int b = 11;
+    int c = a % b;
+    cout << c << endl;
 }
