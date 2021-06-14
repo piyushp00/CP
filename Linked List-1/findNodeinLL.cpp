973719d258d478e79d55308eb5b3870514d2b938
Piyush Pandey <piyushpvns@gmail.com>
2021-06-14 12:22:19 +0530

    Added-Find a Node in LL

---
+++ Linked List-1/findNodeinLL.cpp
@@ -0,0 +1,62 @@
+/* You have been given a singly linked list of integers. 
+Write a function that returns the index/position 
+of an integer data denoted by 'N' (if it exists). Return -1 otherwise. */
+
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+int findNode(Node *head, int num){
+    Node *temp = head;
+    int count = 0;
+    int index = 0;
+    while(temp != NULL){
+        if(temp->data == num){
+            index = count;
+            return index;
+        } 
+        temp = temp->next;
+        count++;
+    
+    }
+    return -1;
+}  
+
+Node *takeInput(){
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1){
+        Node *newNode = new Node(data);
+        if(head == NULL && tail == NULL){
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = tail->next; //or // tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head){
+    Node *temp = head;
+    while(temp !=NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+    temp = head;
+}
+
+int main(){
+    
+    Node *head = takeInput();
+    print(head);
+    int data;
+    cin >> data;
+    int index = findNode(head, data);
+    cout << index << endl;
+}
