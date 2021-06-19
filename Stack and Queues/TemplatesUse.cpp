01052bfafcb2c5dabf4153e1b9121082faa45cb5
Piyush Pandey <piyushpvns@gmail.com>
2021-06-19 12:20:33 +0530

    Templates

--- Stack and Queues/TemplatesUse.cpp
+++ Stack and Queues/TemplatesUse.cpp
@@ -4,7 +4,22 @@
 using namespace std;
 
 int main(){
-    Pair<int> p1;
+
+    Pair<Pair<int,int>, int> p2;
+    p2.setY(10);
+    Pair<int, int> p4;
+    p4.setX(5);
+    p4.setY(16);
+
+    p2.setX(p4);
+    cout << p2.getX().getX() << " " << p2.getX().getY() << " " <<  p2.getY() << endl;
+
+    /* Pair<int, double> p1;
+    p1.setX(100.34);
+    p1.setY(100.34);
+    cout << p1.getX() << " " << p1.getY() << endl; */
+
+    /* Pair<int> p1;
     p1.setX(10);
     p1.setY(20);
 
@@ -13,5 +28,5 @@ int main(){
     Pair<double> p2;
     p2.setX(34.25);
     p2.setY(100.25);
-    cout << p2.getX() << " " << p2.getY() << endl;
+    cout << p2.getX() << " " << p2.getY() << endl; */
 }
