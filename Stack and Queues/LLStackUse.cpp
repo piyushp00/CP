ab04e821e08aa773b0a9237df08ccfeac00601ea
Piyush Pandey <piyushpvns@gmail.com>
2021-06-19 12:20:54 +0530

    Stack Using Liked List

---
+++ Stack and Queues/LLStackUse.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include "LLnode.cpp"
+using namespace std;
+
+int main(){
+    
+    Stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+    cout << s.pop() << endl;
+
+    cout << s.getSize() << endl;
+
+    cout << s.isEmpty() << endl;
+}
