aba36ec6848d1d62319cd560c038100a8c38d6b6
Piyush Pandey <piyushpvns@gmail.com>
2021-06-20 19:39:46 +0530

    Queue Using Array

---
+++ Stack and Queues/QueueArrayClass.h
@@ -0,0 +1,43 @@
+template<typename T> 
+
+class Queue{
+    T *data;
+    int nextIndex;
+    int firstIndex;
+    int size;
+    int capacity;
+
+public:
+    //Constructor
+    Queue(int s){
+        data = new T[s];
+        nextIndex = 0;
+        firstIndex = -1;
+        size = 0;
+        capacity = s;
+    }
+
+    //Size Function
+    int getSize(){
+        return size;
+    }
+
+    //Empty Function
+    bool isEmpty(){
+        return size == 0;
+    }
+
+    //Insert Element
+    void enQueue(T element){
+        if(size == capacity){
+            cout << "Queue Full!" << endl;
+            return; 
+        }
+        data[nextIndex] == element;
+        nextIndex = (nextIndex + 1) % capacity;
+        if(firstIndex == -1){
+            firstIndex = 0;
+        }
+        size++;
+    }
+};
