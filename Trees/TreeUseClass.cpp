6a53774540a410327d249c304975023096510c2e
Piyush Pandey <piyushpvns@gmail.com>
2021-06-22 14:42:56 +0530

    Take Input Level Wise

--- Trees/TreeUseClass.cpp
+++ Trees/TreeUseClass.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 #include "TreeNodeClass.h"
+#include <queue>
 using namespace std;
 
+//Take Input LevelWise
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cout << "Enter Root Data: " << endl;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData); 
+
+    //Make Queue
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    
+    while(pendingNodes.size() == 0){
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter number of childeren of " << front->data << endl;
+        int numChild;
+        cin >> numChild;
+        for(int i = 0; i < numChild; i++){
+            int childData;
+            cout << "Enter " << i << "th child of " << front->data << endl;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+}
+
+//Take Input
 TreeNode<int>* takeInput(){
     int rootData;
     cout << "Enter Data: " << endl;
@@ -18,7 +48,8 @@ TreeNode<int>* takeInput(){
     return root;
 }
 
-void printTree(TreeNode<int>* root){
+//Print Tree
+void printTree(TreeNode<int>* root) {
     //edge case(not base case)
     if(root == NULL){
         return;
