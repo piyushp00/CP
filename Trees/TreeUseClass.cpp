a57fad3baa728dbcd4438359adcef4e4fb91d756
Piyush Pandey <piyushpvns@gmail.com>
2021-06-22 16:45:55 +0530

    Take Input and Print Tree(normal way)

--- Trees/TreeUseClass.cpp
+++ Trees/TreeUseClass.cpp
@@ -3,34 +3,6 @@
 #include <queue>
 using namespace std;
 
-//Take Input LevelWise
-TreeNode<int>* takeInputLevelWise() {
-    int rootData;
-    cout << "Enter Root Data: " << endl;
-    cin >> rootData;
-    TreeNode<int>* root = new TreeNode<int>(rootData); 
-
-    //Make Queue
-    queue<TreeNode<int>*> pendingNodes;
-    pendingNodes.push(root);
-    
-    while(pendingNodes.size() == 0){
-        TreeNode<int>* front = pendingNodes.front();
-        pendingNodes.pop();
-        cout << "Enter number of childeren of " << front->data << endl;
-        int numChild;
-        cin >> numChild;
-        for(int i = 0; i < numChild; i++){
-            int childData;
-            cout << "Enter " << i << "th child of " << front->data << endl;
-            cin >> childData;
-            TreeNode<int>* child = new TreeNode<int>(childData);
-            front->children.push_back(child);
-            pendingNodes.push(child);
-        }
-    }
-}
-
 //Take Input
 TreeNode<int>* takeInput(){
     int rootData;
@@ -74,7 +46,7 @@ int main() {
     root->children.push_back(node1);
     root->children.push_back(node2); */
 
-    TreeNode<int>* root = takeInput();
+    TreeNode<int>* root = takeInputLevelWise() ;
     printTree(root);
     //TODO delete
 }
