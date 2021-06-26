14e636b1b14b3983dceb4da3cb312011f349fe0c
Piyush Pandey <piyushpvns@gmail.com>
2021-06-26 15:21:45 +0530

    Binary Tree Node Class

---
+++ Binary Trees/BinaryTreeNode.cpp
@@ -0,0 +1,21 @@
+template <typename T>
+
+class BinaryTreeNode {
+public:
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+
+    //Constructor
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL:
+    }
+
+    //Destructor
+    ~BinaryTreeNode() {
+        delete left;
+        delete right;
+    }
+};
