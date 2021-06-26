72eaa833e97281121c1d506e0501db9f20f8f330
Piyush Pandey <piyushpvns@gmail.com>
2021-06-26 17:25:03 +0530

    Take Input recursively

--- Binary Trees/BinaryTreeUse.cpp
+++ Binary Trees/BinaryTreeUse.cpp
@@ -2,6 +2,22 @@
 #include "BinaryTreeNode.h"
 using namespace std;
 
+//Take Input
+BinaryTreeNode<int>* takeInput(){
+    int rootData;
+    cout << "Enter Data" << endl;
+    cin >> rootData;
+    if(rootData == -1){
+        return NULL;
+    }
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    BinaryTreeNode<int> *leftChild = takeInput(); 
+    BinaryTreeNode<int> *rightChild = takeInput(); 
+    root->left = leftChild;
+    root->right = rightChild;
+}
+
 //Print Recursively
 void printTree(BinaryTreeNode<int> *root){
     if(root == NULL){
@@ -10,23 +26,29 @@ void printTree(BinaryTreeNode<int> *root){
     
     cout << root->data <<  ":";
     if(root->left != NULL){
-        cout << "L" << root->left->data;
+        cout << "L" << root->left->data << " ";
     }
 
     if(root->right != NULL){
-        cout << "R" << root->right->data;
+        cout << "R" << root->right->data << " ";
     }
     cout << endl;
     printTree(root->left);
     printTree(root->right);
 }
 
+
+
 int main(){
-    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
+    /*
+   
     BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
     BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
+    
     root->left = node1;
     root->right = node2;
+    */
+    BinaryTreeNode<int> *root = takeInput();
     printTree(root);
     delete root;
 }
