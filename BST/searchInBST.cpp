9d7037d15267c6a42db4573b31051e0258b6c1ef
Piyush Pandey <piyushpvns@gmail.com>
2021-07-03 09:11:42 +0530

    Added search in BST

---
+++ BST/searchInBST.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include "BinaryTreeNode.h"
+#include "Node.h"
+#include <vector>
+using namespace std;
+#include <queue>
+
+//Search In BST
+BinaryTreeNode<int>* searchInBST(BinaryTreeNode<int>* root, int x){
+    if(root == NULL){
+        return root;
+    }
+    
+    if(root->data == x){
+        return root;
+    }
+    
+    if(x < root->data){
+       	return searchInBST(root->left, x);
+        
+    } else {
+       	return searchInBST(root->right, x);
+    }
+}
+
+//Print Level at New Line
+void printLevelWise(BinaryTreeNode<int>* root) {
+    if(root == NULL) {
+        return;
+    }
+
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    pendingNodes.push(NULL);
+    
+    while(pendingNodes.size() != 0) {
+        BinaryTreeNode<int>* frontNode = pendingNodes.front();
+        pendingNodes.pop();
+
+        if(frontNode == NULL) {
+            cout << endl;
+            if(!pendingNodes.empty()) {
+                pendingNodes.push(NULL);
+            }
+        } else {
+            cout << frontNode->data << " ";
+
+            if(frontNode->left != NULL) {
+                pendingNodes.push(frontNode->left);
+            }
+
+            if(frontNode->right != NULL) {
+                pendingNodes.push(frontNode->right);
+            }
+        }
+    }
+}
+
+//Print Level Wise
+void printTreeLevelWise(BinaryTreeNode<int> *root) {
+    if(root == NULL) {
+        return;
+    }
+    
+    queue<BinaryTreeNode<int>*> pendingNode;
+    pendingNode.push(root);
+
+    while(pendingNode.size() != 0) {
+        BinaryTreeNode<int>* front = pendingNode.front();
+        pendingNode.pop();
+
+        cout << front->data << ":";
+        if(front->left == NULL) {
+            cout<<"L:-1,";
+        } else {
+            cout << "L:" << front->left->data << ",";
+            pendingNode.push(front->left);
+        }
+
+        if(front->right == NULL) {
+            cout << "R:-1";
+        } else {
+            cout << "R:" << front->right->data;
+            pendingNode.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
+//Take Input Level wise
+BinaryTreeNode<int>* takeInputLevelWise(){
+    int rootData;
+    cout << "Enter Root Data" << endl;
+    cin >> rootData;
+    if(rootData == -1){
+        return NULL;
+    }
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    
+    while(pendingNodes.size() != 0){
+        BinaryTreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter Left Child of " << front->data << endl; 
+        int leftChildData;
+        cin >> leftChildData;
+        if(leftChildData != -1) {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+
+        cout << "Enter Right Child of " << front->data << endl; 
+        int rightChildData;
+        cin >> rightChildData;
+        if(rightChildData != -1) {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+// 1 2 3 4 5 -1 -1 -1 -1 -1 -1
+// 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
+int main(){
+    BinaryTreeNode<int> *root = takeInputLevelWise();
+    int k;
+    cin >> k;
+    cout << ((searchInBST(root, k)) ? "true" : "false");
+    delete root;
+}
