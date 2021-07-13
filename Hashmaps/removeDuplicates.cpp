825ecfe912d4007b8fcc464de59396700b98d0d4
Piyush Pandey <piyushpvns@gmail.com>
2021-07-13 12:23:18 +0530

    Update removeDuplicates.cpp

--- Hashmaps/removeDuplicates.cpp
+++ Hashmaps/removeDuplicates.cpp
@@ -15,9 +15,23 @@ vector<int> removeDuplicates(int* a, int size){
         if(seen.count(a[i]) > 0){
             continue;
         }
+        seen[a[i]] = true;
+        output.push_back(a[i]);
     }
+    return output;
 }
 
 int main(){
+    int size = 0;
+    cin >> size;
+    int *arr = new int[size];
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
 
+    vector<int> v = removeDuplicates(arr, size);
+    for(int i = 0; i < v.size(); i++){
+        cout << v.at(i) << " ";
+    }
+    cout << endl;
 }
