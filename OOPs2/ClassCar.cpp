e78517c11ddc4fbef4f4bb773fe0967371cd3e91
Piyush Pandey <piyushpvns@gmail.com>
2021-09-09 07:22:24 +0530

    Create ClassCar.cpp

---
+++ OOPs2/ClassCar.cpp
@@ -0,0 +1,14 @@
+#include "ClassVehicle.h"
+
+
+class Car : public Vehicle {
+public:
+    int numGears;
+
+    void print(){
+        cout << "Num Tyres: " << numTyres << endl;
+        cout << "Color: " << numTyres << endl;
+        cout << "Num Gears: " << numGears << endl;
+        // cout << "Max Speed: " << maxSpeed << endl; //inaccessible since private properties
+    }
+};
